268e89d79611373e34086ed04dc4ee7ded191f26
glozy <glozy94@gmail.com>
2019-06-24 22:26:27 -0400

    added connection factory

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -8,12 +8,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import com.revature.logging.LoggingUtil;
 
 public class ConnectionFactory {
 	
+	private static String username ;
 	private static String url;
-	private static String user;
 	private static String password;
 	private static final String PROPERTIES_FILE = "src/main/resources/database.properties";
 	private static ConnectionFactory cf;
@@ -25,22 +24,20 @@ public class ConnectionFactory {
 		}
 		
 		return cf.createConnection();
-		
 	}
 	
+	
 	private ConnectionFactory() {
-		//private constructor makes this a singleton
+		//private constructor that makes this a singleton
 		Properties prop = new Properties();
 		try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE)){
 			prop.load(fis);
 			url = prop.getProperty("url");
-			user = prop.getProperty("user");
+			username = prop.getProperty("user");
 			password = prop.getProperty("password");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -49,15 +46,12 @@ public class ConnectionFactory {
 		Connection conn = null;
 		
 		try {
-			conn = DriverManager.getConnection(url, user, password);
-			LoggingUtil.info("Connection to DB Successful.");
-			//System.out.println("Connection Successful");
+			conn = DriverManager.getConnection(url, username, password);
 		} catch (SQLException e) {
-			LoggingUtil.error("Failed to make DB Connection");
+			System.out.println("Failed to make DB Connection");
 		}
 		
 		return conn;
 		
 	}
-
 }
